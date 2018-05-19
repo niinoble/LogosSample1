99ed39efaca5376e88bc26aa8c38fe2d58defb32
niinoble <gbxn@yahoo.com>
2018-05-19 14:32:11 -0400

    Exercise1

---
+++ Day1/testProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace testProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int employeeStatus=1;
+            
+            switch(employeeStatus)
+            {
+                case 1:
+                    Console.WriteLine("Alive");
+                    break;
+                case 2:
+                    Console.WriteLine("Zombie");
+                    break;
+                case 3:
+                    Console.WriteLine("Dead");
+                    break;
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+            }        
+        }
+    }
+}
